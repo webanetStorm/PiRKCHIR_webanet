f5a3f24fcd850ba6e7c264803d587959a97b4f4f
webanet <my2712@mail.ru>
2024-11-18 00:23:28 +0300

    Курсач (начало)

---
+++ Курсовая работа/js/data.js
@@ -0,0 +1,17 @@
+// Данные для отображения
+const studentProfile = {
+    name: "Яскевич Матвей Дмитриевич",
+    group: "ИКБО-21-23",
+    averageGrade: 4.8,
+};
+
+const schedule = [
+    { date: "2024-12-12", subject: "Математика", time: "10:00" },
+    { date: "2024-12-13", subject: "Программирование", time: "13:00" },
+];
+
+const subjects = [
+    { id: 1, name: "Математика", grade: 5, details: "Курс лекций и практик" },
+    { id: 2, name: "Программирование", grade: 4, details: "Курс JavaScript" },
+    { id: 3, name: "Физика", grade: 4, details: "Курс общей физики" },
+];
